a0c8d9083ce4f70671ba287ad169a4d394f62f5b
Wallion Rocha <wallisonrocha2008@gmail.com>
2018-12-02 14:06:21 -0200

    auto instances

--- src/main/java/br/com/cursomc/wallison/CursomcApplication.java
+++ src/main/java/br/com/cursomc/wallison/CursomcApplication.java
@@ -1,12 +1,31 @@
 package br.com.cursomc.wallison;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import br.com.cursomc.wallison.model.Category;
+import br.com.cursomc.wallison.repository.CategoryRepository;
+import br.com.cursomc.wallison.service.CategoryService;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
